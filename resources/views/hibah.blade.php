a25facc00b5fdb8acb2362a022ab2ed37e40fd58
Muhammad Ezra <mezraghalibin@gmail.com>
2016-04-09 11:18:50 +0700

    perubahaan master, dan hibah

--- resources/views/hibah.blade.php
+++ resources/views/hibah.blade.php
@@ -3,16 +3,31 @@
 <html>
 <head>
   <title>HIBAH</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="author" href="humans.txt">
+
+    <!-- CSS FOR PAGE HIBAH -->
     <link rel="stylesheet" href="assets/css/hibah.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:600' rel='stylesheet' type='text/css'>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <!--FOR MATERIALIZE DONT DELETE THIS-->
+      <link href='node_modules/materialize-css/fonts/roboto/' rel='stylesheet' type='text/css'>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+    <!--FOR MATERIALIZE DONT DELETE THIS-->
+
+    <!--FOR BOOTSTRAP DONT DELETE THIS-->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:600' rel='stylesheet' type='text/css'>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!--FOR BOOTSTRAP DONT DELETE THIS-->
+
     <script>
-      $(document).ready(function(){
+    $(document).ready(function(){
         $("#buat-hibah").hide();
 
         $("#kelola").click(function(){
@@ -24,12 +39,92 @@
             $("#buat-hibah").fadeIn(500);
             $("#kelola-hibah").hide();
         });
-      });
+    });
     </script>
 </head>
 <body>
   @section('main_content')
-  <div class="wrapper">
+  <div class="page-content">
+    <nav class="second-navbar">
+      <div class="nav-wrapper">
+        <ul class="left hide-on-med-and-down">
+          <li id="kelola"><a href="#">Kelola Hibah</a></li>
+          <li id="buat"><a href="#">Buat Hibah</a></li>
+        </ul>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#">Login Sebagai muhammad.ezra - Staf Riset</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- CONTENT KELOLA HIBAH -->
+    <div class="container">
+      <div id="kelola-hibah">
+        <div class="header">KELOLA HIBAH</div>
+          <div class="kelola-content">
+            <table class="highlight centered">
+              <thead>
+                <tr>
+                    <th data-field="id">Nama Hibah</th>
+                    <th data-field="name">Kategori</th>
+                    <th data-field="price">Pemberi</th>
+                    <th data-field="price">Besar Dana</th>
+                    <th data-field="price">Periode</th>
+                    <th data-field="price"></th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>Universitas Indonesia Hibah</td>
+                  <td>Hibah</td>
+                  <td>Garuda Indonesia</td>
+                  <td>RP. 150.000.000</td>
+                  <td>11/04/2015 - 11/05/2016</td>
+                  <td>
+                    <a class="waves-effect waves-teal btn-flat">Edit</a>
+                    <a class="waves-effect waves-teal btn-flat">Hapus</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>ABCDEFGHIJK Hibah</td>
+                  <td>Hibah</td>
+                  <td>Garuda Indonesia</td>
+                  <td>RP. 150.000.000</td>
+                  <td>11/04/2015 - 11/05/2016</td>
+                  <td>
+                    <a class="waves-effect waves-teal btn-flat">Edit</a>
+                    <a class="waves-effect waves-teal btn-flat">Hapus</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>ULALALALALA Hibah</td>
+                  <td>Hibah</td>
+                  <td>Garuda Indonesia</td>
+                  <td>RP. 150.000.000</td>
+                  <td>11/04/2015 - 11/05/2016</td>
+                  <td>
+                    <a class="waves-effect waves-teal btn-flat">Edit</a>
+                    <a class="waves-effect waves-teal btn-flat">Hapus</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div>
+    </div>
+      <!-- END OF CONTENT KELOLA HIBAH -->
+
+    <!-- CONTENT BUAT HIBAH -->
+    <div class="container">
+      <div id="buat-hibah">
+        <div class="header">BUAT HIBAH</div>
+      </div>
+    </div>
+    <!-- END OF CONTENT BUAT HIBAH -->
+  </div>
+
+  {{-- <div class="wrapper">
 
   <!-- SECOND NAVBAR -->
     <div class="page-content-wrapper">
@@ -97,7 +192,7 @@
       </div>
       <!-- END OF CONTENT BUAT HIBAH -->
     </div>
-  </div>
+  </div> --}}
   @stop
 </body>
 </html>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,48 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Home</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="author" href="humans.txt">
+	<title>Home</title>
+  	<!--CSS FOR MASTER DONT DELETE THIS -->
     <link rel="stylesheet" href="assets/css/master.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:600' rel='stylesheet' type='text/css'>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <!--FOR MATERIALIZE DONT DELETE THIS-->
+      <link href='node_modules/materialize-css/fonts/roboto/' rel='stylesheet' type='text/css'>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+    <!--FOR MATERIALIZE DONT DELETE THIS-->
+
+    <!--FOR BOOTSTRAP DONT DELETE THIS-->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:600' rel='stylesheet' type='text/css'>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!--FOR BOOTSTRAP DONT DELETE THIS-->
+    <script>
+    //js code
+      $(document).ready(function(){
+         $(".button-collapse").sideNav();
+         $('.collapsible').collapsible();
+      });
+    </script>
 </head>
 <body>
-  <!-- HEADER -->
-  <nav class="navbar navbar-fixed-top shadow-nav">
-    <div class="container-fluid">
-      <div class="col-md-12 navbar-container">
-        <div class="col-md-6 navbar-content">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <img class="image-responsive" src="/images/FIA_UI.png" alt="Logo">
-            </a>
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>                        
-            </button>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right menu">
-              <li><a class="select" href="#">PORTAL</a></li>
-              <li><a class="select" href="#">PESAN</a></li>
-              <li><a class="select" href="#">LOGOUT</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+<!-- HEADER -->
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><img class="responsive-img" src="/images/FIA_UI.png" alt="Logo"></a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">PORTAL</a></li>
+        <li><a href="#">PESAN</a></li>
+        <li><a href="#">LOGOUT</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="#">PORTAL</a></li>
+        <li><a href="#">PESAN</a></li>
+        <li><a href="#">LOGOUT</a></li>
+      </ul>
     </div>
   </nav>
-  <!-- END OF HEADER -->
 
-  <!-- SIDEBAR -->
+  <header>
+    <ul id="nav-mobile" class="side-nav fixed">
+      <li><a href="#" class="waves-effect waves-teal">BERANDA</a></li> <!-- ALL -->
+      <li><a href="#" class="waves-effect waves-teal">PANDUAN</a></li> <!-- Reviewer -->
+      <li><a href="#" class="waves-effect waves-teal">HIBAH</a></li> <!-- Staf Riset & Dosen -->
+      <li><a href="#" class="waves-effect waves-teal">PENGUMUMAN</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">PROPOSAL</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">PROPOSAL HIBAH</a></li> <!-- Staf Keuangan & Dosen-->
+      <li><a href="#" class="waves-effect waves-teal">LAPORAN</a></li> <!-- Dosen & Staf Riset & Reviewer -->
+      <li><a href="#" class="waves-effect waves-teal">KONTAK</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">MOU</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">BORANG</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">REPOSITORY</a></li> <!-- Staf Riset -->
+      <li><a href="#" class="waves-effect waves-teal">KELOLA REPOSITORY</a></li> <!-- Staf Riset -->
+    </ul>
+  </header>
+
+  {{-- <!-- SIDEBAR -->
   <div class="wrapper">
     <div class="sidebar-wrapper shadow-nav-side">
       <ul class="sidebar-nav sidebar-nav-top">
@@ -57,11 +81,9 @@
         <li><a href="#">KONTAK</a></li> <!-- Staf Riset -->
         <li><a href="#">MOU</a></li> <!-- Staf Riset -->
         <li><a href="#">BORANG</a></li> <!-- Staf Riset -->
-        <li><a href="#">REPOSITORY</a></li> <!-- All -->
-        <li><a href="#">KELOLA REPOSITORY</a></li> <!-- Staf Riset -->
       </ul>
     </div>
-  </div>  
+  </div> --}}
   @yield('main_content')
 </body>
 </html>
